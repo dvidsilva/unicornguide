d8152fa3c92f95be4f5ae872b0a01d238e2c7b15
dvid <contacto@dvidsilva.com>
2015-07-28 15:24:28 -0700

    added new admin features

--- admin/scripts/admin.js
+++ admin/scripts/admin.js
@@ -35,8 +35,11 @@ angular.module('unicornguide', ['firebase'])
     _self.loggedIn = true;
   });
 })
-.controller('AccelController' , function (Auth, FB, $firebaseArray, $log) {
+.controller('AccelController' , function (Auth, FB, $firebaseArray, $log, $window) {
   var _self = this;
+
+  _self.action = "create";
+
   _self.new = {};
   Auth.$onAuth(function (authData) {
     if (authData===null) {
@@ -48,11 +51,39 @@ angular.module('unicornguide', ['firebase'])
   });
 
   _self.data = $firebaseArray(FB.child("accelerators"));
-  _self.addNew = function () {
+
+  _self.select = function (a) {
+    _self.action = 'edit';
+    _self.new = _self.data.$getRecord(a.$id);
+  }
+
+  _self.save = function () {
+    $log.info(_self.action);
+    if (_self.action === 'edit') {
+      _self.data.$save(_self.new);
+    } else if (_self.action === 'create'){
+      addNew();
+    }
+  };
+
+  _self.remove = function () {
+    $log.warn('deleting record');
+    $log.info(JSON.stringify(_self.new));
+    if ( $window.confirm("Do you really wanna delete this record?!")) {
+      _self.data.$remove(_self.new);
+    }
+  };
+
+  _self.clearForm = function () {
+    _self.new = {};
+    _self.action = 'create';
+  };
+
+  var addNew = function () {
     $log.info(_self.new);
     _self.data.$add(_self.new);
     _self.new = {};
-  }
+  };
 
 
 });
--- scripts/app.js
+++ scripts/app.js
@@ -1,19 +1,28 @@
 angular.module('unicornguide', ['firebase', 'ui.router'])
 .config(function($stateProvider, $urlRouterProvider) {
   $urlRouterProvider.otherwise("/");
-    $stateProvider
+  $stateProvider
     .state('home', {
       url: "/",
-      templateUrl: "partials/home.html",
+      templateUrl: "/partials/home.html",
       controller: "homeController",
       controllerAs: ""
+    })
+    .state('accelerators', {
+      url: "/accelerators",
+      templateUrl: "/partials/accelerators.html",
+      controller: "accelController",
+      controllerAs: "accelerators"
     })
 })
 .factory('FB', function(){
   return new Firebase("https://unicornguide.firebaseio.com");
 })
-.controller('homeController', function (FB) {
+.controller('homeController', function (FB, $firebaseArray) {
   var _self = this;
   _self.data = $firebaseArray(FB.child("accelerators"));
-
+})
+.controller('accelController', function (FB, $firebaseArray) {
+  var _self = this;
+  _self.data = $firebaseArray(FB.child("accelerators"));
 })
